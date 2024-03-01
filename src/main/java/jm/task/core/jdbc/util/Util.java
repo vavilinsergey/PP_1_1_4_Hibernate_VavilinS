8943455c15a522dcd7cc583490a0c9ea9fdc6fb7
Vavilin Sergei <vavilinsergey@icloud.com>
2024-02-29 17:17:30 -0700

    AllDone
    123

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost/store";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Vdorogevdvoem2!";
+    public static Connection getConnection() {
+
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            return conn;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
